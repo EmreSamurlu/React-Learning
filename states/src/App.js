259fe2d313146e752ff57277b9df059757e5104b
emre <emrsmrl@gmail.com>
2021-09-18 22:13:42 +0300

    React Patika | Object States.

--- states/src/App.js
+++ states/src/App.js
@@ -4,6 +4,7 @@ function App() {
   const [name, setName] = useState('Emre');
   const [age, setAge] = useState(28);
   const [friends, setFriends] = useState(["Ahmet", "Murat"])
+  const [address, setAddress] = useState({ title: 'İzmir', zip: 35000 })
 
   return (
     <div className="App">
@@ -14,12 +15,24 @@ function App() {
       <button onClick={() => setAge(26)} > Change Age </button>
 
       <hr />
+      <br /> <br />
+
       <h2>Friends</h2>
       {friends.map((friend, index) => (
         <div key={index}>{friend}</div>
       ))}
       {/* array içindeki eski değerleri korumak için ...friend ile array eklendi. */}
       <button onClick={() => setFriends([...friends, "Mahmut"])} > Add New Friend </button>
+
+
+      <hr />
+      <br /> <br />
+
+      <h2>Address</h2>
+      <div>{address.title} {address.zip}</div>
+      
+      <button onClick={() => setAddress({...address, title: 'İstanbul' })} > Change Address </button>
+      {/* <button onClick={() => setAddress({title: 'İstanbul', zip: 34000})} > Change Address </button> */}
 
     </div>
   );
