0d0415b012ec2859bb1a47053f377d2249073ec1
emre <emrsmrl@gmail.com>
2021-09-18 20:55:57 +0300

    React Patika | Props Bölüm sonu.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,49 @@
+//* IMPORT *// 
 import "./App.css";
+import User from "./components/User";
 
-import User from "./components/User"
+//******* END *******//
+
+const friends = [
+    {
+        id: 1,
+        name: "Ahmet",
+    },
+    {
+        id: 2,
+        name: "Mehmet",
+    },
+    {
+        id: 3,
+        name: "Cansu",
+    },
+    {
+        id: 4,
+        name: "Evrim",
+    },
+];
+
 //  Boolean veya number gönderirken (line-8) süslü parantezler içinde yazılır. {} //
 function App() {
     return (
         <>
-            <User name="Emre" surname="Samurlu" isLoggedIn={true} age ={28} /> 
+            <User
+                name="Emre"
+                surname="Samurlu"
+                isLoggedIn={true} // comment açıp kapatınca defaultProps kullanımı görünüyor.
+                age="28"
+                friends={friends}
+                address={{
+                    title: "Buca / İzmir",
+                    zip: 35000,
+                }}
+            />
         </>
     );
 }
 
+
+//* EXPORT *//
 export default App;
+
+//******* END *******//
--- hello-react/src/components/User.js
+++ hello-react/src/components/User.js
@@ -1,3 +1,8 @@
+//* IMPORT *//
+import PropTypes from "prop-types";
+
+//******* END *******//
+
 /* function User(props) {
     console.log(props);
     return <h1>
@@ -11,16 +16,46 @@
 
 //* props bir obje olduğu için yukarıdaki yazım şu şekilde düzenlenebilir..
 
-function User({ name, surname, isLoggedIn, age }) {
+function User({ name, surname, isLoggedIn, age, friends, address }) {
 
-    return <h1>
-        {
-            isLoggedIn ? `${name} ${surname} (${age})` : "Giriş Yapmadınız."
-        }
+    if (!isLoggedIn){
+        return <div>Giriş Yapmadınız.</div>
+    }
 
-    </h1>;
+    return (
+        <>
+            <h1>{`${name} ${surname} (${age})`}</h1>
+            {address.title} {address.zip}
+
+            <br/>
+            <br/>
+
+            {
+            friends && 
+            friends.map((friend) => <div key={friend.id}>{friend.name}</div>)
+            }
+        </>
+    );
 }
 
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    friends: PropTypes.array,
+    address: PropTypes.shape({
+        title: PropTypes.string,
+        zip: PropTypes.number,
+    })
+}
+
+User.defaultProps = {
+    name: "İsim Yok!",
+    isLoggedIn: false,
+}
 
 //* EXPORT *//
 export default User;
+
+//******* END *******//
