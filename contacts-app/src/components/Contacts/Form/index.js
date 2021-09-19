aa9f1d833bff2b7d44c07e899a2f72eeb8ab4292
emre <emrsmrl@gmail.com>
2021-09-19 16:04:31 +0300

    React Contact-App | Initial Files.

---
+++ contacts-app/src/components/Contacts/Form/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+            <div>
+            <input name="fullname" />
+            </div>
+            
+            <div>
+            <input name="phone_number" />
+            </div>
+        </div>
+    )
+}
+
+export default Form
---
+++ contacts-app/src/components/Contacts/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import List from "./List";
+import Form from "./Form";
+
+function Contacts() {
+    return (
+        <div>
+            Contacts
+            <List />
+            <Form />
+        </div>
+    )
+}
+
+export default Contacts
