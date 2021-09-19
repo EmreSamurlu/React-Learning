5f489104b2e6ddd30e2eb2e818ea0ea3df109d8b
emre <emrsmrl@gmail.com>
2021-09-19 16:39:03 +0300

    React Contact-App | Kısım 1.

--- contacts-app/src/components/Contacts/Form/index.js
+++ contacts-app/src/components/Contacts/Form/index.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
-function Form() {
+const initialFormValues = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+    const [form, setForm] = useState(initialFormValues)
+
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullname === '' || form.phone_number === '') {
+            return false;
+        }
+        addContact([...contacts, form])
+        console.log(form);
+        // Form gönderildikten sonra input verilerini temizliyoruz. useEffect kullanırsak bu kısmı comment almamız gerekir.
+        // setForm(initialFormValues) 
+    };
+
+    //Yada useEffect ile aynı işlemi yapabiliriz. Bu sayede onSubmit işleminde sadece submit etmiş oluruz.
+    useEffect(() => {
+        setForm(initialFormValues);
+    }, [contacts])
+
     return (
-        <div>
+
+        <form onSubmit={onSubmit} >
             <div>
-            <input name="fullname" />
+                <input name="fullname" placeholder="Full Name" onChange={onChangeInput} value={form.fullname} />
             </div>
-            
+
             <div>
-            <input name="phone_number" />
+                <input name="phone_number" placeholder="Phone Number" onChange={onChangeInput} value={form.phone_number} />
             </div>
-        </div>
+
+            <div>
+                <button  >Add</button>
+            </div>
+
+        </form>
+
     )
 }
 
