d156c643dd55b370b377be371bd2eaf442f81eba
emre <emrsmrl@gmail.com>
2021-09-19 18:39:12 +0300

    React Contact-App | Stil dosyası eklendi.Filter ve map komutları düzenlendi.

--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -1,18 +1,31 @@
 import {useState, useEffect} from 'react'
+import "./style.css"
 
 import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            fullname: "Özge",
+            phone_number: "12346579"
+        },{
+            fullname:"Okan",
+            phone_number: "5465432187"
+        },{
+            fullname:"Mehmet",
+            phone_number: "6579830573"
+        }
+    ]);
 
     useEffect(()=> {
 console.log(contacts);
     }, [contacts])
 
     return (
-        <div>
-            <List />
+        <div id="container">
+            <h1>Contacts</h1>
+            <List contacts={contacts} />
             <Form addContact={setContacts} contacts={contacts} />
         </div>
     )
