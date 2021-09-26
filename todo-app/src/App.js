ef4d4324441c02e04cc6671b95b8be7db051f8b5
emre <emrsmrl@gmail.com>
2021-09-27 01:47:36 +0300

    Todo App update 2

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import clsx from 'clsx';
+
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
 import "./App.css"
 import "./style/style.css"
+// import { style } from '@mui/system';
 
 const App = () => {
   //hooks
@@ -14,19 +24,22 @@ const App = () => {
   const [todoEditing, setTodoEditing] = useState(null)
   const [editingText, setEditingText] = useState("")
 
+  // filter hook
+ 
+
   //use local storage data
-  useEffect(()=> {
+  useEffect(() => {
     const temp = localStorage.getItem("todos")
     const loadedTodos = JSON.parse(temp)
 
-    if (loadedTodos){
+    if (loadedTodos) {
       setTodos(loadedTodos)
     }
   }, [])
-  
-  
+
+
   //save data to local storage
-  useEffect(()=>{
+  useEffect(() => {
     const jsonTemp = JSON.stringify(todos)
     localStorage.setItem("todos", jsonTemp)
   }, [todos])
@@ -36,7 +49,7 @@ const App = () => {
     e.preventDefault();
 
     // giving id to todos is important for differentiate todos
-    const newTodo ={
+    const newTodo = {
       id: new Date().getTime(), // with this variable, we get a unique id for every todo item
       text: todo,
       completed: false,
@@ -48,15 +61,15 @@ const App = () => {
 
   // delete function
   function deleteTodo(id) {
-    const updatedTodos = [...todos].filter((todo)=>todo.id !== id)
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
 
     setTodos(updatedTodos);
   }
 
   // complete function
   function toggleComplete(id) {
-    const updatedTodos = [...todos].map((todo)=>{
-      if(todo.id === id){
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
         todo.completed = !todo.completed
       }
       return todo
@@ -66,10 +79,11 @@ const App = () => {
   }
 
   function editTodo(id) {
-    const updatedTodos = [ ...todos].map((todo)=> {
-      if (todo.id === id){
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
         todo.text = editingText
       }
+      
       return todo;
     })
     setTodos(updatedTodos)
@@ -78,42 +92,59 @@ const App = () => {
     //reseting edits
   }
 
+  // filter todos
+
   return (
     <div className="App">
+      <h2>Yapılacaklar Listesi</h2>
       <form onSubmit={handleSubmit}>
 
         {/* set our todo to what we enter the input area */}
         <input
-        type="text" autoFocus onChange={(e) => setTodo(e.target.value)} value={todo} 
+          className={"add-input"}
+          type="text" autoFocus onChange={(e) => setTodo(e.target.value)} value={todo}
         />
 
-        <button type="submit">Add Todo</button>
+        <IconButton type="submit">
+          <AddCircleOutlineOutlinedIcon />
+        </IconButton>
       </form>
+
       {todos.map((todo) => <div key={todo.id}>
 
-      {/* if we have a todo item we can edit this item, else we can enter an item */}
+        {/* if we have a todo item we can edit this item, else we can enter an item */}
+        {/* we used clsx styling */}
+        <div className={clsx({
+          'completed': todo.completed,
+          'list-item': true,
+        })}>
 
-      {todoEditing === todo.id ? (<input 
-        type="text" onChange={(e)=> setEditingText(e.target.value)} value={editingText} 
-        />) 
-        : (<div>{todo.text}</div>) }
-        
-        
+          <Checkbox onChange={() => toggleComplete(todo.id)} checked={todo.completed}
+          />
 
-
-        <button 
-        onClick={() => deleteTodo(todo.id)}>Delete</button>
-        
-        <input 
-        type="checkbox" onChange={()=> toggleComplete(todo.id)} checked={todo.completed}
-        />
-
-        {todoEditing === todo.id ? (<button onClick={()=>editTodo(todo.id)} >Submit Edit</button>) : (<button onClick={()=>setTodoEditing(todo.id)}>Edit Todo</button>)}
-        
-        
+          {todoEditing === todo.id ? (<input className="edit-text" 
+            type="text" onChange={(e) => setEditingText(e.target.value)} value={editingText}
+          />)
+            : (<div className="todo-item">{todo.text}</div>)}
 
 
-        </div>)}
+          <div className="btn-box">
+
+            <IconButton aria-label="delete" onClick={() => deleteTodo(todo.id)}>
+              <DeleteIcon />
+            </IconButton>
+
+            {todoEditing === todo.id ? (<IconButton aria-label="edit" onClick={() => editTodo(todo.id)}>
+              <SendOutlinedIcon />
+            </IconButton>) : (<IconButton aria-label="edit" onClick={() => setTodoEditing(todo.id)}>
+              <ModeEditOutlineOutlinedIcon />
+            </IconButton>)}
+
+          </div>
+        </div>
+      
+      </div>)}
+
     </div>
   )
 }
